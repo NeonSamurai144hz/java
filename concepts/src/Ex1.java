b1979e90fc3f90eefd73b127c2dbe03df86869e4
Bobeica Nikita <niki.bobeica@gmail.com>
2025-02-12 14:48:43 +0100

    done ex J3

--- concepts/src/Ex1.java
+++ concepts/src/Ex1.java
@@ -23,3 +23,11 @@ public class Ex1 {
 }
 
 // Files attached Animal, Mammifere, Oiseau, Reptile
+
+// 1/ Héritage : Dans un zoo, il existe plusieurs types d'animaux, tels que des mammifères, des oiseaux, et des reptiles. 
+// Tous les animaux ont certaines caractéristiques communes, comme un nom et une méthode pour manger. Cependant, chaque type 
+// d'animal a des comportements spécifiques. Par exemple, les oiseaux volent, tandis que les mammifères marchent, les reptiles rampe.
+
+// Créez une classe abstraite Animal avec un attribut nom et une méthode abstraite seDeplacer().
+// Créez des sous-classes telles que Mammifere, Oiseau et Reptile qui héritent de Animal et redéfinissent la méthode seDeplacer(). 
+// Ajoutez des méthodes spécifiques à chaque type d'animal, par exemple, voler() pour Oiseau et marcher() pour Mammifere.
--- concepts/src/Ex2.java
+++ concepts/src/Ex2.java
@@ -16,3 +16,12 @@ public class Ex2 {
     }
 }
 // Files attached Voiture
+
+// 2/ Encapsulation : Dans une usine de production de voitures, chaque voiture possède des caractéristiques comme le modèle, 
+// la couleur et le prix. Ces informations doivent être encapsulées, c'est-à-dire qu'elles ne doivent être accessibles qu'à 
+// travers des méthodes définies.
+
+// Créez une classe Voiture avec des attributs privés : modele, couleur, prix. 
+// Fournissez des méthodes publiques pour accéder et modifier ces attributs (getters et setters).
+// Assurez-vous que la méthode prix ne peut être modifiée qu'avec une valeur positive. 
+// Ajoutez une méthode afficherDetails() qui affiche les informations sur la voiture.
